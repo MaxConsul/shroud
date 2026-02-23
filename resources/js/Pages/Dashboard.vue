167cddbec67f84aa68615767a7aacaec0188a92c
maxconsul <maxconsul17@gmail.com>
2026-02-23 18:09:23 +0800

    another test

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -23,6 +23,7 @@ import { Head } from '@inertiajs/vue3';
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         You're logged in!
                         Sample changes
+                        changes again
                     </div>
                 </div>
             </div>
