a7547470783464843e124750ffb3cc4bee8c616a
maxconsul17 <mconsul@schools.ph>
2026-02-25 15:34:56 +0800

    deploy again

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -23,7 +23,7 @@ import { Head } from '@inertiajs/vue3';
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         This changes is for testing
                     </div>
-                </div>
+                </div> 
             </div>
         </div>
     </AuthenticatedLayout>
